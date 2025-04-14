0b034b66e9b5ec1516dfb0fb643c003d0e39bd9b
Sebastian <sebas25260104@gmail.com>
2025-04-14 10:43:55 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel'; // Asegúrate de que esta importación existe
-
+import vercel from '@astrojs/vercel';
+// https://astro.build/config
 export default defineConfig({
-  devToolbar: {
-    enabled: false,
-  },
   vite: {
     plugins: [tailwindcss()]
   },
-  site: 'https://sacuervo26.github.io', // Opcional: solo si también usas GitHub Pages
-  base: '/Alma/', // Opcional: solo si usas GitHub Pages (conflicto con Vercel)
+
   adapter: vercel()
 });
